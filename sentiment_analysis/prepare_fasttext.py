008772dc8c91641d6dd0522cfcd161bb12abdd0e
Thomas Hack <th@thomas-hack.net>
2021-08-04 17:14:39 +0200

    Fix prepare fasttext.

--- sentiment_analysis/prepare_fasttext.py
+++ sentiment_analysis/prepare_fasttext.py
@@ -10,6 +10,8 @@ import gensim
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     folder = pathlib.Path(__file__).parent
-    gft_model = gft.load_facebook_vectors(str(folder / 'cc.de.300.bin'))
+    gft_model = gensim.models.fasttext.load_facebook_vectors(
+        str(folder / 'cc.de.300.bin')
+    )
     gft_model.save(str(folder / 'cc.de.300.bin.gensim_model'))
 
